c3ba06e3faaa5fbba202096a737f138653a726ac
mhmHisham <hishamhaneefa96@gmail.com>
2021-06-21 21:38:22 +0530

    Project and Screen setup

---
+++ screens/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View } from 'react-native';
+import { Text } from 'react-native';
+
+const Home = () =>{
+    return(
+
+    <View>
+        <Text>
+            Home
+        </Text>
+    </View>
+    )
+}
+
+export default Home;
---
+++ screens/Resturant.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View,  Text } from 'react-native';
+
+const Resturant = () => {
+    return (
+    <View>
+        <Text>
+            Resturant
+        </Text>
+    </View>
+    )
+}
+
+export default Resturant
